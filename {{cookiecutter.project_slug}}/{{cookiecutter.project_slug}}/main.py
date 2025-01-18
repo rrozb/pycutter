684abe595a97944718740c9143d5a01aaf29570b
Ruslan Rozbeiko <rrozb@gmail.com>
2025-01-18 17:17:54 +0100

    chore: use context manager and organize imports

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main.py
@@ -1,11 +1,12 @@
-{% if cookiecutter.project_type == "api" %}
-import tomllib
+{% if cookiecutter.project_type == "api" %}import tomllib
+
 from fastapi import FastAPI
 
 from {{cookiecutter.project_slug}}.config import settings
 
 _ = settings
-VERSION = tomllib.load(open("pyproject.toml", "rb"))["project"]["version"]
+with open("pyproject.toml", "rb") as f:
+    VERSION = tomllib.load(f)["project"]["version"]
 app = FastAPI(
     title="{{ cookiecutter.project_name }}",
     description="{{ cookiecutter.project_description }}",
